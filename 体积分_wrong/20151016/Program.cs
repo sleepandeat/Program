f916456eab405eab2ee35131aa0cec74c5320a59
sleepandeat <Zminh@live.com>
2015-10-19 20:57:42 +0800

    20151019

---
+++ 体积分_wrong/20151016/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20151016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            double x, y;
+            double xlb = 1, xub = 0, ylb = 1, yub = 0;
+            double sum = 0;
+            while(xlb>xub||ylb>yub)
+            {
+                while(flag)
+                {
+                    try
+                    {
+                        xlb = Convert.ToDouble(Console.ReadLine());
+                        flag = !flag;
+                    }
+                    catch
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                flag = !flag;
+                while (flag)
+                {
+                    try
+                    {
+                        xub = Convert.ToDouble(Console.ReadLine());
+                        flag = !flag;
+                    }
+                    catch
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                flag = !flag;
+                while (flag)
+                {
+                    try
+                    {
+                        ylb = Convert.ToDouble(Console.ReadLine());
+                        flag = !flag;
+                    }
+                    catch
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                flag = !flag;
+                while (flag)
+                {
+                    try
+                    {
+                        yub = Convert.ToDouble(Console.ReadLine());
+                        flag = !flag;
+                    }
+                    catch
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                flag = !flag;
+                x = xlb;
+                y = ylb;
+                while(x<=xub)
+                {
+                    while(y<=yub)
+                    {
+                        sum += 0.000001 * Math.Sin(y) + y * Math.Sin(0.000001);
+                        y += 0.000001;
+                    }
+                    x += 0.000001;
+                }
+                Console.WriteLine(sum);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 体积分_wrong/20151016/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("20151016")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20151016")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05cf2fd6-28c4-45b8-ae9b-635e615c55b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
