d528b1a4acb213decddb33e53be03584f7ca556a
sleepandeat <Zminh@live.com>
2015-10-24 09:36:33 +0800

    20151024

---
+++ 16进制图片/16进制图片/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Command1_Click()
+    End Sub
+    Private Sub Command1_Click()
+        Dim HEXSTR As String
+        Dim A() As Byte
+        HEXSTR = Text1
+        ReDim A(Len(HEXSTR) / 2)
+        For I = 0 To Len(HEXSTR) Step 2
+            A(I / 2) = Val("&H" & Mid(HEXSTR, I + 1, 2))
+        Next
+        Open "c:\123.PNG" For Binary As #1
+Put #1, , A()
+Close #1
+End Sub
+
+End Module
